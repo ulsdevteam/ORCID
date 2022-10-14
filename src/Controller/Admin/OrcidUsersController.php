726c4eeda253316fefdc62f9e531fee08432540e
Tyler Lendon <trl75@pitt.edu>
2022-10-14 17:02:27 -0400

    Needs better trusted page

--- src/Controller/Admin/OrcidUsersController.php
+++ src/Controller/Admin/OrcidUsersController.php
@@ -310,11 +310,11 @@ class OrcidUsersController extends AppController
                         }
                     }
                     // Ask if the user meant to do that
-                    $html = array(
+                    /*$html = array(
                         'header' => 'ORCID@Pitt Trusted Party Status',
                         'p' => array('You chose not to grant trusted party status to Pitt, thus not allowing the university to access your ORCID iD.', 'Allowing Pitt to be a trusted party will help you and the university maintain accurate records of your research outputs within Pitt systems such as the Faculty Information System and D-Scholarship@Pitt.', 'If you would like to grant Pitt trusted party status or add information to your ORCID profile, please <a href="/connect">restart this process</a>.', 'To find out more about the ORCID@Pitt initiative and the benefits of having an ORCID iD, please visit the <a href="http://www.library.pitt.edu/orcid">ORCID@Pitt website.</a>'),
-                    );
-                    require('../includes/template.php');
+                    );*/
+                    $this->viewBuilder()->setTemplatePath("connect")->setTemplate('untrusted')->setLayout('public');
                     exit();
                 default:
                     // ORCID could send a different error message, but that isn't handled (yet)
