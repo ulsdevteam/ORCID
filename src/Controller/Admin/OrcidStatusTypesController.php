32109b91c81ec26dec387e433c8882e40eaa15b4
Tyler Lendon <trl75@pitt.edu>
2023-01-09 11:35:10 -0500

    Removed all unneeded functions

--- src/Controller/Admin/OrcidStatusTypesController.php
+++ src/Controller/Admin/OrcidStatusTypesController.php
@@ -36,72 +36,10 @@ class OrcidStatusTypesController extends AppController
      */
     public function view($id = null)
     {
-        $orcidStatusType = $this->OrcidStatusTypes->get($id);
-
-        $this->set(compact('orcidStatusType'));
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $orcidStatusType = $this->OrcidStatusTypes->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $orcidStatusType = $this->OrcidStatusTypes->patchEntity($orcidStatusType, $this->request->getData());
-            if ($this->OrcidStatusTypes->save($orcidStatusType) !== false ) {
-                $this->Flash->success(__('The orcid status type has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The orcid status type could not be saved. Please, try again.'));
-        }
-        $this->set(compact('orcidStatusType'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Orcid Status Type id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
         $orcidStatusType = $this->OrcidStatusTypes->get($id, [
-            'contain' => [],
+            'contain' => ['CurrentOrcidStatuses', 'CurrentOrcidStatuses.OrcidUsers']
         ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $orcidStatusType = $this->OrcidStatusTypes->patchEntity($orcidStatusType, $this->request->getData());
-            if ($this->OrcidStatusTypes->save($orcidStatusType) !== false ) {
-                $this->Flash->success(__('The orcid status type has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The orcid status type could not be saved. Please, try again.'));
-        }
         $this->set(compact('orcidStatusType'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Orcid Status Type id.
-     * @return \Cake\Http\Response|null|void Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $orcidStatusType = $this->OrcidStatusTypes->get($id);
-        if ($this->OrcidStatusTypes->delete($orcidStatusType)) {
-            $this->Flash->success(__('The orcid status type has been deleted.'));
-        } else {
-            $this->Flash->error(__('The orcid status type could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
